02f44554766e441fa13c9666983c92e92e9923a0
peter-sharp <Peter@petersharp.co.nz>
2017-05-08 22:29:27 +1200

    converted to a factory

--- src/prefabs/soldiers.js
+++ src/prefabs/soldiers.js
@@ -1,17 +1,12 @@
 import Phaser from 'phaser';
 //Documentation for Phaser's (2.6.2) sprites:: phaser.io/docs/2.6.2/Phaser.Sprite.html
-class Soldiers extends Phaser.Sprite {
-
-  //initialization code in the constructor
-  constructor(game, x, y, frame) {
-    super(game, x, y, 'soldiers', frame);
-  }
-
-  //Code ran on each frame of game
-  update() {
+function soldiers(game, x, y, player){
+  var state = {};
+  state.player = player;
+  state.sprite = player.create(x, y, 'soldiers');
+  return {
 
   }
-
 }
 
-export default Soldiers;
+export default soldiers;
