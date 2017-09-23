d1185ef80e553791ab7363aa2892be0a92f18747
Peter Sharp <peter@webmad.co.nz>
2017-09-23 15:37:53 +1200

    fixed world position

--- src/prefabs/soldiers.js
+++ src/prefabs/soldiers.js
@@ -1,13 +1,16 @@
 import Phaser from 'phaser';
 import {selectable} from 'prefabs/selectable'
 //Documentation for Phaser's (2.6.2) sprites:: phaser.io/docs/2.6.2/Phaser.Sprite.html
-function soldiers(game, x, y, player){
+function soldiers( map, mapX, mapY, player){
   var state = {};
   state.player = player;
-  state.sprite = player.create(x, y, 'soldiers');
+  const worldX = mapX * map.tileWidth;
+  const worldY = mapY * map.tileHeight;
+  debugger;
+  state.sprite = player.create(worldX, worldY, 'soldiers');
   return Object.assign(
     {},
-    selectable(state)
+    selectable( state)
   );
 }
 
