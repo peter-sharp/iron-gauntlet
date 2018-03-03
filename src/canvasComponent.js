736244abfe656b9a4324c6b981251356e3d27dfe
Peter Sharp <peter.victor.sharp@gmail.com>
2018-03-03 18:32:32 +1300

    started canvas component

--- src/canvasComponent.js
+++ src/canvasComponent.js
@@ -1,2 +1,21 @@
 import html from 'choo/html'
 import Nanocomponent from 'nanocomponent'
+
+export function CanvasComponent () {
+  if(!(this instanceof CanvasComponent)) return new CanvasComponent()
+  Nanocomponent.call(this)
+  this.ctx = null
+}
+
+CanvasComponent.prototype = Object.create(Nanocomponent.prototype)
+CanvasComponent.prototype.constructor = Nanocomponent
+
+CanvasComponent.prototype.createElement = function() {
+  var canvas = html`<canvas></canvas>`
+  this.ctx = canvas.getContext('2d')
+  return canvas
+}
+
+CanvasComponent.prototype.update = function() {
+  return html`<canvas></canvas>`
+}
