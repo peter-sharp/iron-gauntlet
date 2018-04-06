bf5ba5d4eb0d709904860aabd643e42421c255fd
Peter Sharp <peter.victor.sharp@gmail.com>
2018-04-06 20:45:00 +1200

    created startCurrentGame

--- src/gameStore.js
+++ src/gameStore.js
@@ -1,4 +1,5 @@
 import game from './game'
+import curry from 'lodash/fp/curry'
 
 export function getCurrentGame(state) {
   return state.gamesIndexed[state.params.game]
@@ -8,44 +9,41 @@ export function updateCurrentGame(state, game) {
   return state.gamesIndexed[state.params.game] = game
 }
 
+const startCurrentGame = curry(function (state, events, game) {
+
+  events.emit(state.events.ADD_GAME, game)
+  state.currentGame = game;
+  events.emit(state.events.PUSHSTATE, `/games/${game.id}`)
+})
+
 function gameStore(socket, state, events) {
   state.games = []
   state.gamesIndexed = {}
 
   state.currentGame = null;
 
-  state.events.ADD_GAME = 'addGame'
-  state.events.CREATE_GAME = 'createGame'
-  state.events.JOIN_GAME = 'joinGame'
-  state.events.JOINED_GAME = 'joinedGame'
-  state.events.GAME_CREATED = 'gameCreated'
+  state.events.ADD_GAME           = 'addGame'
+  state.events.CREATE_GAME        = 'createGame'
+  state.events.JOIN_GAME          = 'joinGame'
+  state.events.JOINED_GAME        = 'joinedGame'
+  state.events.GAME_CREATED       = 'gameCreated'
   state.events.UPDATE_MAX_PLAYERS = 'updateMaxPlayers'
 
   events.on(state.events.CREATE_GAME, function createNewGame() {
 
     socket.emit(state.events.CREATE_GAME, game({ownerId: state.currentPlayer.id}))
-    socket.on(state.events.GAME_CREATED, game => {
-
-      events.emit(state.events.ADD_GAME, game)
-      state.currentGame = game;
-      events.emit(state.events.PUSHSTATE, `/games/${game.id}`)
-    })
+    socket.on(state.events.GAME_CREATED, startCurrentGame(state, events))
   })
 
   events.on(state.events.JOIN_GAME, function joinGame(id) {
     socket.emit(state.events.JOIN_GAME, id, state.currentPlayer)
-    socket.on(state.events.JOINED_GAME, game => {
-
-      events.emit(state.events.ADD_GAME, game)
-      state.currentGame = game;
-      events.emit(state.events.PUSHSTATE, `/games/${game.id}`)
-    })
+    socket.on(state.events.JOINED_GAME, startCurrentGame(state, events))
   })
 
   events.on(state.events.UPDATE_MAX_PLAYERS, maxPlayers => {
-    var game = Object.assign({}, getCurrentGame(state))
+    var game = Object.assign({}, state.currentGame)
     game.maxPlayers = maxPlayers
-    updateCurrentGame(state, game)
+    state.currentGame = game
     events.emit(state.events.RENDER)
   })
 
@@ -64,4 +62,5 @@ function gameStore(socket, state, events) {
     events.emit(state.events.RENDER)
   })
 }
+
 export default gameStore
