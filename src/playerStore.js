53ee3d130a322eab213b1999b630868d2afa5ec2
peter-sharp <Peter@petersharp.co.nz>
2018-07-09 22:07:48 +1200

    added players prop, use existing current player if available

--- src/playerStore.js
+++ src/playerStore.js
@@ -6,11 +6,12 @@ function playerStore (state, events) {
   state.events.SET_PLAYERS = 'setPlayers';
   state.events.UPDATE_CURRENT_PLAYER = 'updateCurrentPlayer';
   state.events.UPDATED_CURRENT_PLAYER = 'updatedCurrentPlayer';
-  state.currentPlayer =  Player()
+  state.currentPlayer =  state.currentPlayer || Player()
+  state.players = []
 
   events.on(state.events.UPDATE_CURRENT_PLAYER, function updateCurrentPlayer(update) {
     state.currentPlayer = Object.assign(state.currentPlayer, update)
-    
+
     events.emit(state.events.UPDATED_CURRENT_PLAYER, update)
     events.emit(state.events.RENDER)
   })
