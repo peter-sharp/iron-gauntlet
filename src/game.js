3b747b4f2cab48e887ce9d19c8799831f3c6fe4e
peter-sharp <Peter@petersharp.co.nz>
2018-07-09 22:06:43 +1200

    only player ids added to game

--- src/game.js
+++ src/game.js
@@ -32,26 +32,34 @@ function Game({
   this.visibility = visibility || 'public'
 }
 
-Game.addPlayer = function(game, player) {
-  if(Game.getPlayer(game, player)) return game
-  game.players.push(player)
+Game.addPlayer = function(game, playerId) {
+  if(Game.hasPlayer(game, playerId)) return game
+  if(isObject(playerId)) playerId = playerId.id
+  game.players.push(playerId)
   return game
 }
 
 Game.removePlayer = function(game, playerId) {
   if(isObject(playerId)) playerId = playerId.id
-  let idx = game.players.findIndex(player => player.id === playerId)
+  let idx = game.players.findIndex(player => player === playerId)
   game.players.splice(idx, 1)
   return game
 }
 
+// TODO this function is now useless
 Game.getPlayer = function(game, playerId) {
   if(isObject(playerId)) playerId = playerId.id
-  return game.players.find(player => player.id === playerId)
+  return game.players.find(player => player === playerId)
 }
 
-Game.isOwner = function(game, player) {
-  return game.ownerId == player.id
+Game.hasPlayer = function(game, playerId) {
+  if(isObject(playerId)) playerId = playerId.id
+  return game.players.indexOf(playerId) > -1
+}
+
+Game.isOwner = function(game, playerId) {
+  if(isObject(playerId)) playerId = playerId.id
+  return game.ownerId == playerId
 }
 
 module.exports = Game
